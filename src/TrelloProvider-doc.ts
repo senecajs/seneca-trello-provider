abbd9fce5d3af149deec69b9157f3a97b8f1f84c
Alex Milenkovic <jk1143298@gmail.com>
2023-06-13 18:09:38 +0200

    commit

--- src/TrelloProvider-doc.ts
+++ src/TrelloProvider-doc.ts
@@ -1,16 +1,34 @@
 /* Copyright © 2022 Seneca Project Contributors, MIT License. */
 
 
-const docs = {
+const messages = {
 
   get_info: {
     desc: 'Get information about the Trello SDK.',
   },
 
+  save_board: {
+    desc: 'Save/Update Trello data into an entity',
+  },
+  load_board: {
+    desc: 'Load Trello data into an entity',
+  },
+  list_board: {
+    desc: 'List Trello data into an entity',
+  },
+
 }
 
-export default docs
+const sections = {}
+
+export default {
+  messages,
+  sections
+}
 
 if ('undefined' !== typeof (module)) {
-  module.exports = docs
+  module.exports = {
+    messages,
+    sections
+  }
 }
